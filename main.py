68e3b751497ae7ecb159de3fa0815a42a3a2be0a
JakobSefovSin <edward.alderson.mr.robot@gmail.com>
2019-09-26 09:27:36 +0200

    Added collection table. Support saving cards to collection.

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ def create_connection(db_file):
 
 	try:
 		conn = sqlite3.connect(db_file)
+		conn.execute("PRAGMA foreign_keys = 1")
 		return conn
 	except sqlite3.Error as e:
 		#TkMessagebox
@@ -66,6 +67,7 @@ def query_all(conn):
 	return cur.fetchall()
 
 def query_by_name(conn, name, table="Card"):
+	#TODO: change from fstring to cur.execute
 	sql = f"""SELECT * FROM {table} WHERE name=\'{name}\'"""
 		
 	cur = conn.cursor()
@@ -76,13 +78,13 @@ def query_by_name(conn, name, table="Card"):
 	return cur
 
 def sql_setup():
-	DB = "testdb.db"
+	DB = "collection.db"
 
-	sql_collection_table = """ CREATE TABLE IF NOT EXISTS collection (
-									id integer PRIMARY KEY,
-									name text NOT NULL,
-									price text,
-									image blob
+	sql_collection_table = """ CREATE TABLE IF NOT EXISTS Collection (
+									internal_id integer PRIMARY KEY,
+									card_data_id integer NOT NULL,
+									foreign key (card_data_id)
+										references Card (id)
 								);"""
 
 	conn = create_connection(DB)
@@ -94,6 +96,8 @@ def sql_setup():
 class CollectionWindow(tk.Frame):
 	def __init__(self, parent, *args, **kwargs):
 		tk.Frame.__init__(self, parent, *args, **kwargs)
+
+		sql_setup()
 
 		self.parent = parent
 			
@@ -120,9 +124,9 @@ class CollectionWindow(tk.Frame):
 
 
 	def create_buttons(self):
-		conn = create_connection("testdb.db")
+		conn = create_connection("collection.db")
 		cur = conn.cursor()
-		cur.execute("SELECT * FROM collection;")
+		cur.execute("SELECT * FROM Collection;")
 		for row in cur:
 			i, name, price, img = row
 			if not img: continue
@@ -136,9 +140,27 @@ class MainWindow(tk.Frame):
 	#TODO: deprecate self.currentCard (will only be saving relation)
 	
 	#class variable ˇ samo enkrat, za vse classe
-	DB = "testdb.db"
-	ALL_CARDS_DB = "minimal.db"
-	
+	DB = "collection.db"
+
+	def _create_connection(self):
+		#Deprecated
+		print("_create_connection method is deprecatedT")
+		sql_setup()
+
+		conn = create_connection(self.ALL_CARDS_DB)
+		
+		sql = """ ATTACH DATABASE (?) AS collection;"""
+
+		cur = conn.cursor()
+
+		cur.execute(sql, (self.DB,))
+
+		test = cur.execute("""SELECT * FROM Collection;""")
+		
+		return conn
+		print("Conn created")
+
+
 	def __init__(self, parent, *args, **kwargs):
 		tk.Frame.__init__(self, parent, *args, **kwargs)
 		#super
@@ -161,7 +183,7 @@ class MainWindow(tk.Frame):
 		self.searchTxt.grid(row=0, column=0,sticky=tk.W+tk.E+tk.N+tk.S)
 
 		tk.Button(self.parent, text="Search SF", command=self.search_card_scryfall).grid(row=0, column=1,sticky=tk.W+tk.E+tk.N+tk.S)
-		tk.Button(self.parent, text="Save to DB", command=self.push_to_collection).grid(row=1, sticky=tk.W+tk.E+tk.N+tk.S)
+		tk.Button(self.parent, text="Save to DB", command=self.foo).grid(row=1, sticky=tk.W+tk.E+tk.N+tk.S)
 		tk.Button(self.parent, text='Quit', command=self.parent.destroy).grid(row=1, column=1, sticky=tk.W+tk.E+tk.N+tk.S)
 
 		tk.Button(self.parent, text='C', command=self.create_window).grid(row=2, column=0, sticky=tk.W+tk.E+tk.N+tk.S)
@@ -172,12 +194,14 @@ class MainWindow(tk.Frame):
 		Searches card in local db and displays it as a button.
 
 		Uses image bytes in db to display image. Will find and save image if not exists.
+		
+		Crashes if no card found.
 		"""
 
 		start = time.time()
 
 		query = self.searchTxt.get()
-		conn = create_connection(self.ALL_CARDS_DB)
+		conn = create_connection(self.DB)
 
 		results = query_by_name(conn, query)
 		
@@ -192,18 +216,23 @@ class MainWindow(tk.Frame):
 
 		except OSError:
 			#TODO: multy faced cards
+
+			start = time.time()
 			front_image_bytes = requests.get(front_image_link).content
+			end = time.time()
+			print(f"Image download time: {end-start}")
+
 			self.currentCard.image = ImageTk.PhotoImage(Image.open(BytesIO(front_image_bytes)))
 			self.insert_image_bytes(id_, front_image_bytes, back_image_bytes)
 
 
-		tk.Button(self.parent, image=self.currentCard.image).grid(row=3, rowspan=2)
+		tk.Button(self.parent, image=self.currentCard.image, command=lambda:self.push_to_collection(id_)).grid(row=3, rowspan=2)
 
 		#id, name, *rest = results.fetchall()[0]
 		#tk.Button(self.parent, text=name, command=lambda:print(rest)).grid(row=3, rowspan=2)
 
 	def insert_image_bytes(self, id_, front_image_bytes, back_image_bytes):
-		conn = create_connection(self.ALL_CARDS_DB)
+		conn = create_connection(self.DB)
 		sql = """ UPDATE Card 
 				  SET front_image=?, back_image=? where id is ?;"""
 
@@ -235,20 +264,24 @@ class MainWindow(tk.Frame):
 		tk.Label(self.parent, text=f"{self.currentCard.price}\u20AC").grid(row=4, column=1)
 
 
-	def push_to_collection(self):
-		#TODO: collection will only be a pointer
+	def push_to_collection(self, all_cards_id):
+		print("HERE")
+		print(all_cards_id)
+
+		#TODO: collection will only be a "pointer"
 		conn = create_connection(self.DB)
 
-		sql = """INSERT INTO collection(name, price, image)
-				 VALUES(?,?,?)"""
-
-		cardData = (self.currentCard.name, self.currentCard.price, self.currentCardImageBytes)
+		sql = """INSERT INTO Collection(card_data_id)
+				 VALUES(?)"""
 		
 		cur = conn.cursor()
-		cur.execute(sql, cardData)
+		cur.execute(sql, (all_cards_id,))
 		conn.commit()
 
 		print(cur.lastrowid)
+
+	def foo(self):
+		print("bar")
 
 	def test(self, tkConfigureEvent):
 		print(tkConfigureEvent)
@@ -265,6 +298,7 @@ if __name__ == '__main__':
 	"""TODO:
 	-save img to db on img click
 	-!one to many relation
+	-separate class for search function
 	"""
 	sql_setup()
 
